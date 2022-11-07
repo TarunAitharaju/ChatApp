5e1f6773749c163b76379ef6a5f15f5d0b672f59
TarunAitharaju <tarunaitharaju1699@gmail.com>
2022-11-07 11:36:17 +0530

    new changes

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 const PORT = 3231;
-app.use(express.static(__dirname + "/../../build"));
+app.use(express.static(__dirname + "/build"));
 io.on("connection", (socket) => {
   const id = socket.handshake.query.id;
   socket.join(id);
